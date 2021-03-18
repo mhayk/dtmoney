5899f87769b6bd9814a1426bad1872f6c5f4bedc
Mhayk Whandson <hi@mhayk.com>
2021-03-18 17:46:44 +0000

    Buttons of (out/in)come have been transformed in styled compoenent.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,13 +2,16 @@ import Modal from 'react-modal';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg';
-import { Container, TransactionTypeContainer } from './styles'
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
+import { useState } from 'react';
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit');
+
     return (
         <Modal
             isOpen={isOpen}
@@ -31,19 +34,23 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <input type="number" placeholder="Value" />
 
                 <TransactionTypeContainer>
-                    <button
+                    <RadioBox
                         type="button"
+                        onClick={() => setType('deposit')}
+                        isActive={type === 'deposit'}
                     >
                         <img src={incomeImg} alt="Income" />
                         <span>Income</span>
-                    </button>
+                    </RadioBox>
 
-                    <button
+                    <RadioBox
                         type="button"
+                        onClick={() => setType('withdraw')}
+                        isActive={type === 'withdraw'}
                     >
                         <img src={outcomeImg} alt="Outcome" />
                         <span>Outcome</span>
-                    </button>
+                    </RadioBox>
                 </TransactionTypeContainer>
 
                 <input placeholder="Category" />
