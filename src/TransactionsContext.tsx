a9f943218a6063322dfc81ad5b594c5f925f1998
Mhayk Whandson <hi@mhayk.com>
2021-03-19 20:43:01 +0000

    A new method named createTransaction has been created into the Transaction context.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -10,11 +10,30 @@ interface Transaction {
     createdAt: string;
 }
 
+// interface TransactionInput {
+//     title: string;
+//     amount: number;
+//     type: string;
+//     category: string;
+//     createdAt: string;
+// }
+
+// type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category'>;
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => void;
+}
+
 interface TransactionsProviderProps {
     children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+export const TransactionsContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -24,8 +43,12 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
             .then(response => setTransactions(response.data.transactions))
     }, []);
 
+    function createTransaction(transaction: TransactionInput) {
+        api.post('/transactions', transaction)
+    }
+
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{ transactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>
     )
